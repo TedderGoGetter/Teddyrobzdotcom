8764a08d548ef425299f7d8cf3b87f1cafefc090
Teddy Robinson <contactteddyrobinson@gmail.com>
2023-08-31 08:59:11 +0200

    design phase started

--- src/Plan.jsx
+++ src/Plan.jsx
@@ -1,8 +1,48 @@
+import constructionGif from './assets/construction.gif'
+import './Plan.css'
+
 function Plan() {
 
     return (
         <>
-        hi
+            hi <br></br>
+            <br></br>
+            This website is very much still under construction, and I'm just a junior engineer.
+            <br></br><br></br>
+            Me working on this code:
+            <br></br>
+
+            <img src={constructionGif} className='construction-gif' alt="construction-gif" />
+
+            <br></br><br></br>
+            So what am I doing here?
+            <br></br><br></br>
+            Well, this morning I had the bright idea to make a music player as a portfolio website.
+            <br></br><br></br>
+            ...even though I have no clue how to make a music player as a portfolio website.
+            <br></br><br></br>
+            But I suppose that's why it'll be a good challenge.
+            <br></br><br></br>
+            And the other bright idea I had was to plan it out ON the website. So without further ado:
+            <br></br><br></br>
+            TEDDY. MAKES. A MUSIC WEBAPP.(make this text look cool later)
+            <br></br><br></br>
+            <br></br><br></br>
+            <h2>Chapter 1: Initial design</h2>
+            Since the world is very much mobile now I will optimize for *mobile first*.
+            <br></br><br></br>
+            *What do I want from this music player?*
+            <br></br>
+            I want to make a music player that has my latest release. 
+            Not only can people play the song but they can also add effects to it.
+            The effects should always sound good, there should be no way for the person messing with it to make it ugly.
+            After all, it's my job as a music producer and an engineer to curate this experience.
+            <br></br>
+            
+            `
+
+
+
         </>
     )
 }
