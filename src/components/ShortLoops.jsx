b72b230e02f5101243d82999f15d1419dae45613
Teddy Robinson <contactteddyrobinson@gmail.com>
2023-09-07 13:28:30 +0200

    sampler is working with vercel host

--- src/components/ShortLoops.jsx
+++ src/components/ShortLoops.jsx
@@ -1,15 +1,21 @@
 import * as Tone from 'tone';
 import Drums from '../assets/NPL1drms.mp3'
+import Instruments from '../assets/NPL1inst.mp3'
+import Sparkles from '../assets/NPL1sprk.mp3'
 
 function ShortLoops() {
 
+    // It's possible that Vercel will only host files if the import is justified. If so keep these in the code:
+    // new Audio(Drums)
+    // new Audio(Instruments)
+    // new Audio(Sparkles)
+
     const sampler = new Tone.Sampler({
         urls: {
-            A1: "NPL1drms.mp3",
+            A1: "NPL1drms-1b4c525a.mp3",
             // A2: "A2.mp3",
         },
-        // baseUrl: "https://tonejs.github.io/audio/casio/",
-        baseUrl: "../assets/"
+        baseUrl: "https://teddyrobz.vercel.app/assets/",
 
     }).toDestination();
 
@@ -21,27 +27,25 @@ function ShortLoops() {
     // repeated event every 8th note
     Tone.Transport.scheduleRepeat((time) => {
         // use the callback time to schedule events
-        osc.start(time).stop(time + 0.1);
+        sampler.start(time).stop(time + 0.1);
     }, "8n");
     // transport must be started before it starts invoking events
 
     function PressPlay() {
         Tone.start()
-        // sampler.triggerAttackRelease(["C1"], 0.5);
-        // Tone.Transport.nstart();
+        sampler.triggerAttackRelease(["A1"], 10);
+        // Tone.Transport.start();
         console.log('note played')
-        
-        new Audio(Drums).play();
         
     }
 
     function PressStop() {
-        Tone.Transport.stop();
+        // Tone.Transport.stop();
+        sampler.stop()
     }
 
     return (
         <>
-        Hello
         <button onClick={PressPlay}>Play</button>
         <button onClick={PressStop}>Stop</button>
         </>
