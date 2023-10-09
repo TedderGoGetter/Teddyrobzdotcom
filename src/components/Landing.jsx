8fa58a2c5986e2e7aaf4c390b858dcd60e37d80d
Teddy Robinson <contactteddyrobinson@gmail.com>
2023-10-09 14:33:56 +0200

    start of overhaul

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -3,7 +3,6 @@ import './Landing.css'
 
 function Landing(){
 
-
     return (
         <>
         <a href='https://ffm.to/rissastrikapose' target='_blank' className='cover-art'><img src={Coverart} className='cover-art'alt='Rissastrikapose Cover Art'></img></a>
