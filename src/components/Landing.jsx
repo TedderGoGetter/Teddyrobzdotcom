5f13a45f03bfe52e193f14e42395dbedfb3eae42
Teddy Robinson <contactteddyrobinson@gmail.com>
2023-10-10 12:38:04 +0200

    very minimal for now

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -4,9 +4,9 @@ import './Landing.css'
 function Landing(){
 
     return (
-        <>
+        <div className="main-container">
         <a href='https://ffm.to/rissastrikapose' target='_blank' className='cover-art'><img src={Coverart} className='cover-art'alt='Rissastrikapose Cover Art'></img></a>
-        </>
+        </div>
     )
 }
 
